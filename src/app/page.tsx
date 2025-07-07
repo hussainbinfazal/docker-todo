da008d86ad36fd461df67866d9cb9414ed6e0715
hussainbinfazal <mdhussain0610@gmail.com>
2025-07-07 21:29:57 +0530

    updated docker compose dev file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import axios from "axios";
